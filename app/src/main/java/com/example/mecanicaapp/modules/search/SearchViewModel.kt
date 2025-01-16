f87d506de7dbec6ed0f1804944319baaef20acfd
DESKTOP-K734R8R\pc <proveedor501@culqi.com>
2025-01-15 19:25:46 -0500

    update data local file

--- app/src/main/java/com/example/mecanicaapp/modules/search/SearchViewModel.kt
+++ app/src/main/java/com/example/mecanicaapp/modules/search/SearchViewModel.kt
@@ -1,48 +1,16 @@
 package com.example.mecanicaapp.modules.search
 
 import androidx.lifecycle.ViewModel
-import com.example.mecanicaapp.data.model.AutoPartModel
 import com.example.mecanicaapp.data.repository.local.MecanicaLocalRepositoryImpl
+import com.example.mecanicaapp.util.DataLocal
 
 class SearchViewModel(
     private val repositoryImpl: MecanicaLocalRepositoryImpl
 ) : ViewModel() {
 
-    companion object {
-        val AUTO_PARTS = listOf(
-            AutoPartModel(
-                0,
-                "D1543-8751",
-                "PK47",
-                "HYUNDAI ACCENT, RIO",
-                "S/.60.94",
-                "S/.93.76",
-                "S/.150.00"
-            ),
-            AutoPartModel(
-                0,
-                "D1950-9175",
-                "PT39",
-                "TOYOTA YARIS ,ENVIDIA",
-                "S/.49.35",
-                "S/.75.93",
-                "S/.150.00"
-            ),
-            AutoPartModel(
-                0,
-                "D696-7570",
-                "PH20",
-                "CHEVROLET N300, GEELY  CK D684",
-                "S/.53.53",
-                "S/.82.36",
-                "S/.180.00"
-            ),
-        )
-    }
-
     suspend fun saveData() {
         if (!repositoryImpl.getConfig()) {
-            repositoryImpl.save(AUTO_PARTS)
+            repositoryImpl.save(DataLocal.AUTO_PARTS)
             repositoryImpl.saveConfig()
         }
     }
---
+++ app/src/main/java/com/example/mecanicaapp/util/DataLocal.kt
@@ -0,0 +1,35 @@
+package com.example.mecanicaapp.util
+
+import com.example.mecanicaapp.data.model.AutoPartModel
+
+object DataLocal {
+    val AUTO_PARTS = listOf(
+        AutoPartModel(
+            0,
+            "D1543-8751",
+            "PK47",
+            "HYUNDAI ACCENT, RIO",
+            "S/.60.94",
+            "S/.93.76",
+            "S/.150.00"
+        ),
+        AutoPartModel(
+            0,
+            "D1950-9175",
+            "PT39",
+            "TOYOTA YARIS ,ENVIDIA",
+            "S/.49.35",
+            "S/.75.93",
+            "S/.150.00"
+        ),
+        AutoPartModel(
+            0,
+            "D696-7570",
+            "PH20",
+            "CHEVROLET N300, GEELY  CK D684",
+            "S/.53.53",
+            "S/.82.36",
+            "S/.180.00"
+        ),
+    )
+}
